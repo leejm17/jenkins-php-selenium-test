53b6c22e4c60e97cdf668325a485218ae93a28d7
Jun Ming Lee <lee_jm0117@hotmail.com>
2022-11-16 22:34:04 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,8 +2,7 @@
 
 set -x
 #docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-#docker run -d -p 80:80 --name my-apache-php-app -v "C:\\Users\\lee_j\\Desktop\\University\\SIT (FT)\\2022-2023\\Y5T1\\ICT3203\\Lab\\X07b - Integrating Jenkins with Automated Integration and UI Testing\\jenkins-php-selenium-test\\jenkins-php-selenium-test\\src://var/www/html" php:7.2-apache
-docker run -d -p 80:80 --name my-apache-php-app -v "//C//Users//lee_j//Desktop//University//SIT (FT)//2022-2023//Y5T1//ICT3203//Lab//X07b - Integrating Jenkins with Automated Integration and UI Testing//jenkins-php-selenium-test//jenkins-php-selenium-test//src://var//www//html" php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v "C:\\Users\\lee_j\\Desktop\\University\\SIT (FT)\\2022-2023\\Y5T1\\ICT3203\\Lab\\X07b - Integrating Jenkins with Automated Integration and UI Testing\\jenkins-php-selenium-test\\jenkins-php-selenium-test\\src://var/www/html" php:7.2-apache
 sleep 1
 set +x
 
